c9abea550c18adcd85a5a630b0162fa7e0fc6744
Nitin Sharma <nsharma78@gmail.com>
2018-01-21 12:49:55 -0800

    PeakElement implemented

--- src/main/java/com/nitin/algo/PeakElement.java
+++ src/main/java/com/nitin/algo/PeakElement.java
@@ -7,5 +7,26 @@
 package com.nitin.algo;
 
 public class PeakElement {
+	public static void main(String[] args) {
+		int[] arr = {10, 9, 15, 2, 23, 90, 67};
+		System.out.println("The peak elelment in the array is: " + findPeak(arr));
+	}
+
+	private static int findPeak(int[] arr) {
+		int l = 0, r = arr.length - 1;
+		
+		while(l <= r) {
+			int mid = l + (r - l)/2;
+			  if ((mid == 0 || arr[mid-1] <= arr[mid]) && (mid == arr.length -1 ||  arr[mid+1] <= arr[mid]))
+			            return arr[mid];
+			else if(arr[mid] < arr[mid -1]) {
+				r = mid - 1;
+			}
+			else if(arr[mid] > arr[mid -1]) {
+				l = mid + 1;
+			}
+		}
+		return -1;
+	}
 
 }
