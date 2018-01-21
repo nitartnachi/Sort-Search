4c4d7dc823230e4894f03d848d17f6e86380ac19
Nitin Sharma <nsharma78@gmail.com>
2018-01-21 11:40:54 -0800

    MergeSort implemented

---
+++ src/main/java/com/nitin/algo/PeakElement.java
@@ -0,0 +1,11 @@
+/*'
+ * Find a peak element
+ * Given an array of integers. Find a peak element in it. An array element is peak if it is NOT smaller than its neighbors. For corner elements, we need to consider only one neighbor. 
+ * For example, for input array {5, 10, 20, 15}, 20 is the only peak element. For input array {10, 20, 15, 2, 23, 90, 67}, there are two peak elements: 20 and 90. 
+ * Note that we need to return any one peak element.
+ */
+package com.nitin.algo;
+
+public class PeakElement {
+
+}
